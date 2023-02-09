d5169d56625642d3ef00fcb6e34388859ebb98a1
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2023-02-09 12:01:04 +0100

    Update CuckooClockRenderer.java

--- src/main/java/com/sammy/clockwork_creations/client/CuckooClockRenderer.java
+++ src/main/java/com/sammy/clockwork_creations/client/CuckooClockRenderer.java
@@ -19,19 +19,30 @@ public class CuckooClockRenderer extends ClockBlockEntityRenderer{
 
     @Override
     public void renderMovingClockParts(ClockBlockEntity blockEntity, ResourceLocation texture, Direction direction, PoseStack poseStack) {
+        float rotationTime = 24000;
+        float time = Minecraft.getInstance().level.getTimeOfDay(Minecraft.getInstance().getDeltaFrameTime())*rotationTime;
+
+
+        renderHand(texture, direction, poseStack, time*0.05f, 1.01f); //Hour Hand
+        renderHand(texture, direction, poseStack, time*0.3f-150f, 1.02f); //Minute Hand
+    }
+
+    public static void renderHand(ResourceLocation texture, Direction direction, PoseStack poseStack, float rotation, float zLevel) {
         VertexConsumer consumer = RenderHandler.DELAYED_RENDER.getBuffer(LodestoneRenderTypeRegistry.TRANSPARENT_TEXTURE.applyAndCache(texture));
+        float trueRotation = ((rotation % 300) / 300f) * 360f;
+
 
         float start = -0.5f;
         float end = 0.5f;
 
-        Vector3f[] positions = new Vector3f[]{new Vector3f(start, start-0.25f, 1.01f), new Vector3f(end, start-0.25f, 1.01f), new Vector3f(end, end-0.25f, 1.01f), new Vector3f(start, end-0.25f, 1.01f)};
+        Vector3f[] positions = new Vector3f[]{new Vector3f(start, start-0.25f, zLevel), new Vector3f(end, start-0.25f, zLevel), new Vector3f(end, end-0.25f, zLevel), new Vector3f(start, end-0.25f, zLevel)};
 
         poseStack.pushPose();
         poseStack.translate(0.5f, 0.5f, 0.5f);
         poseStack.mulPose(Vector3f.YN.rotationDegrees(direction.toYRot()));
         poseStack.translate(0, -0.0625f,-0.375f);
 
-        poseStack.mulPose(Vector3f.ZP.rotationDegrees(blockEntity.getLevel().getGameTime()+ Minecraft.getInstance().getDeltaFrameTime()));
+        poseStack.mulPose(Vector3f.ZN.rotationDegrees(trueRotation));
 
         VFXBuilders.createWorld()
                 .setPosColorTexLightmapDefaultFormat()
